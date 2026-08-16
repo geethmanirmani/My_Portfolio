55a8f933ddaab3be4cddfb65e022cd42277f5ded
Geethma Nirmani <geethmanirmani2601@gmail.com>
2026-08-16 16:49:10 +0530

    Set automatic basePath for GitHub Pages subpath deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: isGithubActions ? "/My_Portfolio" : "",
   trailingSlash: true,
 };
 
